5400b57ee9b67ec18070ab54633f797ba8001f70
Vliet,Matthijs M.B. van <matthijs.vanvliet@student.fontys.nl>
2021-05-27 14:28:15 +0200

    Added VariableDeclarationListener.java

---
+++ VariableDeclarationListener.java
@@ -0,0 +1,28 @@
+public class VariableDeclarationListener extends QwertyBaseListener
+{
+    private SymbolTable symbolTable;
+
+    public void SetSymbolTable(SymbolTable symbolTable_)
+	{
+		symbolTable = symbolTable_;
+	}
+
+    @Override
+    public void exitVariable_declaration(QwertyParser.Variable_declarationContext ctx)
+    {
+        String type = ctx.type().getText();
+        String name = ctx.VARNAME().getText();
+        QwertyParser.Value_expressionContext value = ctx.value_expression();
+
+        SymbolTableEntry entry = SymbolTableEntry.CreateVariable(type, name, value);
+
+        try
+        {
+            symbolTable.AddEntry(entry);
+        }
+        catch (SymbolTable.VariableAlreadyExistsException ex)
+        {
+            System.out.println(ex);
+        }
+    }
+}
